91ed1628b4051b602ef83951881cb9c212d8e198
Victoria <153975106+victoriammmm@users.noreply.github.com>
2026-07-30 15:54:32 +0700

    Add files via upload

---
+++ rest_server.h
@@ -0,0 +1,19 @@
+#ifndef REST_SERVER_H
+#define REST_SERVER_H
+
+#include "postgres.h"
+
+typedef const char *(*endpoint_handler)(const char *method, const char *body, void *user_data);
+
+typedef struct
+{
+    const char *url;
+    endpoint_handler handler;
+
+} Endpoint;
+
+extern void rest_init(void);
+extern void register_endpoint(const char *url, endpoint_handler handler);
+extern void rest_server_poll(void);
+
+#endif
