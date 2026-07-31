809c13bd26c9acd38400b31391ab1c540f48e07c
Victoria <153975106+victoriammmm@users.noreply.github.com>
2026-07-31 11:20:11 +0700

    Update rest_server.h

--- contrib/my_extension/rest_server.h
+++ contrib/my_extension/rest_server.h
@@ -2,6 +2,7 @@
 #define REST_SERVER_H
 
 #include "postgres.h"
+#include <stdbool.h>
 
 typedef const char *(*endpoint_handler)(const char *method, const char *body, void *user_data);
 
@@ -11,6 +12,19 @@ typedef struct
     endpoint_handler handler;
 
 } Endpoint;
+
+typedef struct
+{
+    bool active;
+    int fd;
+    char response[4096];
+    size_t response_len;
+    char read_buffer[4096];
+    size_t read_pos;
+    size_t written;
+    bool response_ready;
+
+} Client;
 
 extern void rest_init(void);
 extern void register_endpoint(const char *url, endpoint_handler handler);
